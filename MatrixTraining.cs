b1466dd068437e9c1e3bdf1b63568f30223589c1
Ivan <ivan.naumov92@yahoo.com>
2018-06-24 16:48:15 +0300

    Import

---
+++ MatrixTraining.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DescendingMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sizeOfMatrix = int.Parse(Console.ReadLine());
+
+            int[,] matrix = GetMatrix(sizeOfMatrix);
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static int[,] GetMatrix(int size)
+        {
+            var matrix = new int[size, size];
+
+            for (int i = 0, col = i + 1; i < matrix.GetLength(0); i++, col = i + 1)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++, col++)
+                {
+                    matrix[i, j] = col;
+                }
+                
+            }
+
+            return matrix;
+        }
+    }
+}
